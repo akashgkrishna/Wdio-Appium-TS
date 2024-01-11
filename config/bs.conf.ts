e1a7f8e90c781802caee4b03e7093c98cf0ddf03
akashkrishna7 <akashkg@testvagrant.com>
2024-01-11 14:42:43 +0530

    changed bs config

--- config/bs.conf.ts
+++ config/bs.conf.ts
@@ -1,40 +1,37 @@
-// import { Logger } from '../test/reporting/logger';
-// exports.config = {
-//     user: process.env.BROWSERSTACK_USERNAME || 'priyankahs_ESljGO',
-//     key: process.env.BROWSERSTACK_ACCESS_KEY || 'qTpfkmcJsugKDHyShJ5t',
-//     hostname: 'hub.browserstack.com',
-//     services: [
-//       [
-//         'browserstack',
-//         {
-//           app: 'bs://557ea258f0c4b9233af2588d4bbfc05dabfa6445',
-//           // buildIdentifier: "${DATE_TIME}",
-//           browserstackLocal: true
-//         },
-//       ]
-//     ],
-//     capabilities: [{
-//       'bstack:options': {
-//         deviceName: 'Samsung Galaxy S22 Ultra',
-//         platformVersion: '12.0',
-//         platformName: 'android',
-//       }
-//     }, 
-//   ],
-//     commonCapabilities: {
-//       'bstack:options': {
-//         projectName: "BrowserStack Sauce Labs App",
-//         buildName: "bstack-sauceLabs",
-//         debug: true,
-//         // networkLogs: true
-//       }
-//     },
-//     maxInstances: 1,
-//     specs: [
-//       '/Users/testvagrant/Documents/Krishna/WDIO/TS_PRAC_WDIO/test/spec/logout/positiveLogout.test.ts'
-//   ],
-//   logLevel: 'silent',
-//   reporters: [
-//     Logger
-//   ],
-// }
+import { Logger } from '../test/reporting/logger';
+exports.config = {
+    user: process.env.BROWSERSTACK_USERNAME || 'elastictest_IEsUKn',
+    key: process.env.BROWSERSTACK_ACCESS_KEY || '96tSpGcY9aYpcdQWPuWD',
+    hostname: 'hub.browserstack.com',
+    services: [
+      [
+        'browserstack',
+        {
+          app: 'bs://aa25b129c9b06533674b464fd7ef540baee887cd',
+          // buildIdentifier: "${DATE_TIME}",
+          browserstackLocal: true
+        },
+      ]
+    ],
+    capabilities: [{
+      'bstack:options': {
+        deviceName: 'Samsung Galaxy S22 Ultra',
+        platformVersion: '12.0',
+        platformName: 'android',
+      }
+    }, 
+  ],
+    commonCapabilities: {
+      'bstack:options': {
+        projectName: "BrowserStack Sauce Labs App",
+        buildName: "bstack-sauceLabs",
+        debug: true,
+        // networkLogs: true
+      }
+    },
+    maxInstances: 1,
+    specs: [
+      '/Users/testvagrant/Documents/Krishna/WDIO/TS_PRAC_WDIO/test/spec/logout/positiveLogout.test.ts'
+  ],
+  logLevel: 'info',
+}
