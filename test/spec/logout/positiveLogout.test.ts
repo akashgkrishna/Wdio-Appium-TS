ac9896eaa1d7c14e5c2513ad8e8c77d7ed6b832f
akashkrishna7 <akashkg@testvagrant.com>
2024-01-22 15:49:53 +0530

    Added more methods for baseScreen

--- test/spec/logout/positiveLogout.test.ts
+++ test/spec/logout/positiveLogout.test.ts
@@ -1,19 +1,26 @@
+import { Logger } from "../../customLogger/logger";
 import { LoginScreen } from "../../screens/loginScreen";
 import { LogoutScreen } from "../../screens/logoutScreen";
 let logoutScreen: LogoutScreen;
 let loginScreen: LoginScreen;
+let specName = 'Logout flow'
+const LOGGER = new Logger();
 
-describe('Logout test', function(){
+describe(specName, function(){
     before(async function() {
         logoutScreen = new LogoutScreen();
         loginScreen = new LoginScreen();
         await loginScreen.performLogin();
-
+        LOGGER.info(`Spec Name: ${specName}`);
     })
+
+    this.beforeEach(async function () {
+        LOGGER.info(`Test Name: ${this.currentTest?.title}`);
+    })
+
 
     it('Logout positive flow', async function(){
 
         await logoutScreen.logout();
-
     })
 })
