20ede548e3573df72d5be630e983f57b1b51e539
akashkrishna7 <akashkg@testvagrant.com>
2024-01-30 19:26:16 +0530

    Added empty cart tests

--- test/screens/cartScreen.ts
+++ test/screens/cartScreen.ts
@@ -1,13 +1,18 @@
+import { Logger } from "../customLogger/logger";
 import { ProductDetails } from "../resources/customTypes/productDetails";
 import { BaseScreen } from "./base/baseScreen"
-
+const LOGGER = new Logger();
 export class CartScreen extends BaseScreen{
     private selectors = {
 
         proceedToCheckoutButton: `//android.widget.TextView[@text="Proceed To Checkout"]`,
         productRows: `//android.view.ViewGroup[@content-desc="product row"]`,
         productName: `//android.view.ViewGroup[@content-desc="product row"]//android.widget.TextView[@content-desc="product label"]`,
-        productPrice: `//android.view.ViewGroup[@content-desc="product row"]//android.widget.TextView[@content-desc="product price"]`
+        productPrice: `//android.view.ViewGroup[@content-desc="product row"]//android.widget.TextView[@content-desc="product price"]`,
+        removeIndividualProductButton: `(//android.view.ViewGroup[@content-desc="remove item"])[1]`,
+        noItems: `//android.widget.TextView[@text="Oh no! Your cart is empty. Fill it up with swag to complete your purchase."]`,
+        quantityMinus: `(//android.view.ViewGroup[@content-desc="counter minus button"])[1]/android.widget.ImageView`,
+        quanityPlus: `(//android.view.ViewGroup[@content-desc="counter plus button"])[##PLACEHOLDER##]/android.widget.ImageView`
       
     }
 
@@ -46,5 +51,26 @@ export class CartScreen extends BaseScreen{
                 cartProduct.productName === product.productName && cartProduct.productPrice === product.productPrice
             )
         );
+    }
+
+    async removeProductsFromCartIndividually(){
+        const numberOfProducts = await this.getNumberOfProductsInCart();
+        if(numberOfProducts>0){
+            for(let i = 0; i < numberOfProducts; i++)
+            this.click(this.selectors.removeIndividualProductButton);
+        }
+        else{
+            LOGGER.info('The cart is already empty. No products to remove.')
+        }
+    }
+
+    async isCartEmpty():Promise<boolean>{
+       return this.isDisplayed(this.selectors.noItems);
+    }
+
+    async removeProductsFromCartByReducingQuantity(){
+        while(await this.isExisting(this.selectors.quantityMinus)){
+            await this.click(this.selectors.quantityMinus);
+        }
     }
 }
